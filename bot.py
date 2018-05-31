22b1aece1b4048d4668ecce4d7416836d5d45455
Dmitriy Yurkin <dmitrii.iurkin@yandex.ru>
2018-05-31 16:55:43 +0300

    telegram: added start and hello functions

--- bot.py
+++ bot.py
@@ -1,7 +1,17 @@
-import config
+import config 
 import telebot
 
 bot = telebot.TeleBot(config.token)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    sent = bot.send_message(message.chat.id, 'Как тебя зовут?')
+    bot.register_next_step_handler(sent, hello)
+
+def hello(message):
+    bot.send_message(
+        message.chat.id,
+        'Привет, {name}. Рад тебя видеть.'.format(name=message.text))
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
