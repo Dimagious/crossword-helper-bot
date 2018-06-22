4d509b659bd1f2cd4a9ab13085bdc5b0a7fe779d
Dmitriy Yurkin <dmitrii.iurkin@yandex.ru>
2018-06-22 21:22:00 +0300

    added Procfile

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
 # bot
-TOKEN = ''
+TOKEN = '609304918:AAGGhFv0sl4o6gisjKgR6C5S7-mYVnQQBno'
+TELEGRAM = 'https://api.telegram.org/bot609304918:AAGGhFv0sl4o6gisjKgR6C5S7-mYVnQQBno/'
 
 # database
-DATABASELINK = ""
-HOST = ''
-USER = ''
-PASSWORD = ''
-DATABASE = ''
-PORT = ''
+DATABASELINK = "postgres://username:password@host:port/database"
+HOST = 'localhost'
+USER = 'postgres'
+PASSWORD = 'aquiva'
+DATABASE = 'Words'
+PORT = '5432'
 
 # urls for parser
-URL_FOR_WORD = ''
-URL_FOR_DESCRIPTION = ''
+URL_FOR_WORD = 'http://loopy.ru/?word='
+URL_FOR_DESCRIPTION = 'http://loopy.ru/?word=&def='
