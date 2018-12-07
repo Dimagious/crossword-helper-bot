989efc3bb614497c8f07ed5623681d827a0c4c18
Dmitriy <dmitrii.iurkin@yandex.ru>
2018-12-07 13:11:09 +0300

    added empty config file

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # bot
-TOKEN = '609304918:AAGGhFv0sl4o6gisjKgR6C5S7-mYVnQQBno'
-PORT = '8443'
+TOKEN = ''
+PORT = ''
 
 # urls for parser
-URL_FOR_WORD = 'http://loopy.ru/?word='
-URL_FOR_DESCRIPTION = 'http://loopy.ru/?word=&def='
-TEST_URL = 'https://www.stihi.ru'
-WRONG_URL = 'https://www.stiref35gf35fhi.ru'
+URL_FOR_WORD = ''
+URL_FOR_DESCRIPTION = ''
+TEST_URL = ''
+WRONG_URL = ''
