a3f4f815e9351814a644c022e4378f290f7b64e5
Dmitriy <dmitrii.iurkin@yandex.ru>
2018-12-07 10:42:00 +0300

    changed webhook

--- bot.py
+++ bot.py
@@ -67,7 +67,6 @@ def error(bot, update, error):
 
 
 def main():
-    PORT = int(os.environ.get('PORT', '5000'))
     updater = Updater(config.TOKEN, request_kwargs=PROXY)
 
     dp = updater.dispatcher
@@ -86,8 +85,8 @@ def main():
     dp.add_error_handler(error)
 
     updater.start_webhook(listen="0.0.0.0",
-                          port=PORT,
-                          url_path="https://mycrosswordbot.herokuapp.com/" + config.TOKEN)
+                          port=5000,
+                          webhook_url="https://mycrosswordbot.herokuapp.com:5000/" + config.TOKEN)
     updater.idle()
 
 
